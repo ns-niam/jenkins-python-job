e417eef071027333014bcd67f92846b20a6b1666
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-02-22 10:59:47 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,9 +1,1 @@
-import sys
-
-if len(sys.argv) < 2:
-    print("Error: No names provided.")
-    sys.exit(1)
-
-print("Names from command-line arguments:")
-for name in sys.argv[1:]:
-    print(name)
+print("Md Shs Niamatullah"
